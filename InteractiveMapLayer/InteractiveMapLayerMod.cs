f14aba232418cd7706bcd6ed856a7e1975aa72f8
Platonymous <david@millionideas.de>
2017-05-05 01:43:14 +0200

    Gave More Map Layers it's own mod

--- InteractiveMapLayer/InteractiveMapLayerMod.cs
+++ InteractiveMapLayer/InteractiveMapLayerMod.cs
@@ -41,32 +41,12 @@ namespace InteractiveMapLayer
         {
             reset();
 
-            Game1.mapDisplayDevice = new mapDisplayDeviceIntercept((xTile.Display.XnaDisplayDevice)Game1.mapDisplayDevice);
-            DrawMapEvents.DrawMapLayer += DrawMapEvents_DrawMapLayer;
-
             LocationEvents.CurrentLocationChanged += LocationEvents_CurrentLocationChanged;
             GameEvents.UpdateTick += GameEvents_UpdateTick;
             SaveEvents.BeforeSave += (x, y) => reset();
             ControlEvents.KeyPressed += ControlEvents_KeyPressed;
             
 
-        }
-
-        private void DrawMapEvents_DrawMapLayer(object sender, DrawLayerEventArgs e)
-        {
-            if (e.PriorLayerID == "Back" && e.NewLayerID == "Buildings")
-            {
-                int i = 0;
-                while (Game1.currentLocation.Map.Layers[i].Id != "Buildings")
-                {
-                    if (Game1.currentLocation.Map.Layers[i].Id != "Back")
-                    {
-                        Game1.currentLocation.Map.Layers[i].Draw(Game1.mapDisplayDevice, Game1.viewport, Location.Origin, false, Game1.pixelZoom);
-                    }
-
-                    i++;
-                }
-            }
         }
 
         private void reset()
