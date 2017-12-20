1d3cc6652295adf0ffee1613ff9c2d801e029636
Platonymous <david@millionideas.de>
2017-12-20 21:59:53 +0100

    switch to using visualize

---
+++ Portraiture/PortaitureVHandler.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Visualize;
+using StardewValley;
+using StardewValley.Menus;
+
+namespace Portraiture
+{
+    class PortaitureVHandler : IVisualizeHandler
+    {
+        public bool Begin(ref SpriteBatch __instance, ref SpriteSortMode sortMode, ref BlendState blendState, ref SamplerState samplerState, ref DepthStencilState depthStencilState, ref RasterizerState rasterizerState, ref Effect effect, ref Matrix transformMatrix)
+        {
+            return true;
+        }
+
+        public bool Draw(ref SpriteBatch __instance, ref Texture2D texture, ref Vector4 destination, ref bool scaleDestination, ref Rectangle? sourceRectangle, ref Color color, ref float rotation, ref Vector2 origin, ref SpriteEffects effects, ref float depth)
+        {
+
+            if (TextureLoader.activeFolder <= 0 || !(Game1.activeClickableMenu is ShopMenu || Game1.activeClickableMenu is DialogueBox) || texture.Width != 128)
+                return true;
+
+            NPC npc = getNPCForTexture(texture);
+
+            if (npc == null)
+                return true;
+            Texture2D newTexture = TextureLoader.getPortrait(npc.name);
+
+            if (newTexture == null)
+                return true;
+
+            texture = newTexture;
+            sourceRectangle = new Rectangle?(TextureLoader.getSoureRectangle(texture));
+            destination.W = 64 * Game1.pixelZoom;
+            destination.Z = 64 * Game1.pixelZoom;
+            scaleDestination = false;
+
+            PortraitureMod.activeTexure = texture;
+            return true;
+        }
+
+        public NPC getNPCForTexture(Texture2D texture)
+        {
+            foreach (NPC npc in Utility.getAllCharacters())
+                if (npc.Portrait == texture)
+                    return npc;
+
+            return null;
+        }
+    }
+}
--- Portraiture/PortraitureMod.cs
+++ Portraiture/PortraitureMod.cs
@@ -6,19 +6,19 @@ using StardewModdingAPI.Events;
 using StardewValley;
 using StardewValley.Menus;
 using System;
-using System.Collections.Generic;
 using System.IO;
+using Visualize;
 
 namespace Portraiture
 {
     public class PortraitureMod : Mod
     {
-        private ShopMenu activeShop;
-        private DialogueBox activeDialogueBox;
         private float displayAlpha;
         public static IModHelper helper;
         private static Mod instance;
         internal static PConfig config;
+        internal static Texture2D activeTexure;
+        private static IVisualizeHandler vHandler = new PortaitureVHandler();
         
         public override void Entry(IModHelper help)
         {
@@ -27,14 +27,21 @@ namespace Portraiture
             config = Helper.ReadConfig<PConfig>();
             string customContentFolder = Path.Combine(helper.DirectoryPath, "Portraits");
             displayAlpha = 0;
+            VisualizeMod.addHandler(vHandler);
             
-
             if (!Directory.Exists(customContentFolder))
-            {
                 Directory.CreateDirectory(customContentFolder);
-            }
 
             SaveEvents.AfterLoad += SaveEvents_AfterLoad;
+            SaveEvents.AfterReturnToTitle += SaveEvents_AfterReturnToTitle;
+        }
+
+        private void SaveEvents_AfterReturnToTitle(object sender, EventArgs e)
+        {
+            GameEvents.FourthUpdateTick -= GameEvents_FourthUpdateTick;
+            MenuEvents.MenuClosed -= MenuEvents_MenuClosed;
+            MenuEvents.MenuChanged -= MenuEvents_MenuChanged;
+            ControlEvents.KeyPressed -= ControlEvents_KeyPressed;
         }
 
         public static void log (string text)
@@ -46,26 +53,27 @@ namespace Portraiture
         {
             TextureLoader.loadTextures();
             GameEvents.FourthUpdateTick += GameEvents_FourthUpdateTick;
-            MenuEvents.MenuChanged += MenuEvents_MenuChanged;
             MenuEvents.MenuClosed += MenuEvents_MenuClosed;
+            MenuEvents.MenuChanged += MenuEvents_MenuChanged;
             ControlEvents.KeyPressed += ControlEvents_KeyPressed;
         }
 
         private void GameEvents_FourthUpdateTick(object sender, System.EventArgs e)
         {
-            displayAlpha -= 0.05f;
+            displayAlpha = Math.Max(displayAlpha - 0.05f, 0);
         }
 
-        private void drawFolderName(SpriteBatch b, int textureSize, int x, int y)
+        private void drawFolderName(SpriteBatch b, int x, int y)
         {
+            if (displayAlpha <= 0)
+                return;
+
             string activeFolderText = TextureLoader.getFolderName();
 
             activeFolderText = activeFolderText.Replace('_', ' ');
 
-            if (textureSize > 64)
-            {
+            if (activeTexure is Texture2D texture && texture.Width > 128)
                 activeFolderText = "= " + activeFolderText;
-            }
 
             int textlength = (int)Game1.smallFont.MeasureString(activeFolderText).X;
             int textheight = (int)Game1.smallFont.MeasureString(activeFolderText).Y;
@@ -78,54 +86,7 @@ namespace Portraiture
 
             IClickableMenu.drawTextureBox(b, Game1.menuTexture, new Rectangle(0, 256, 60, 60), (int)displayBoxPos.X, (int)displayBoxPos.Y, displayBoxWidth, displayBoxHeight, Color.White * displayAlpha, 1f, false);
             if (displayAlpha >= 0.8)
-            {
                 Utility.drawTextWithShadow(b, activeFolderText, Game1.smallFont, new Vector2(displayBoxPos.X + ((displayBoxWidth - textlength) / 2), Game1.pixelZoom + displayBoxPos.Y + ((displayBoxHeight - textheight) / 2)), Game1.textColor);
-            }
-
-        }
-
-        private void drawPortraiture(SpriteBatch b, DialogueBox d, NPC cs)
-        {
-            if (d.width < 107 * Game1.pixelZoom * 3 / 2 || Helper.Reflection.GetPrivateValue<bool>(d, "transitioning") || Helper.Reflection.GetPrivateValue<bool>(d, "isQuestion"))
-            {
-                return;
-            }
-            
-            Texture2D texture = TextureLoader.getPortrait(cs.name);
-            
-            Dialogue characterDialogue = Helper.Reflection.GetPrivateValue<Dialogue>(d, "characterDialogue");
-            int textureSize = Math.Max(texture.Width / 2, 64);
-
-            int x = Helper.Reflection.GetPrivateValue<int>(d, "x");
-            int y = Helper.Reflection.GetPrivateValue<int>(d, "y");
-
-            int num1 = x + d.width - 112 * Game1.pixelZoom + Game1.pixelZoom;
-            int num2 = x + d.width - num1;
-            int num3 = num1 + Game1.pixelZoom * 19;
-            int num4 = y + d.height / 2 - 74 * Game1.pixelZoom / 2 - 18 * Game1.pixelZoom / 2;
-
-            int num5 = Helper.Reflection.GetPrivateMethod(d, "shouldPortraitShake").Invoke<bool>(new object[] { characterDialogue }) ? Game1.random.Next(-1, 2) : 0;
-
-            Rectangle rectangle = Game1.getSourceRectForStandardTileSheet(texture, characterDialogue.getPortraitIndex(), textureSize, textureSize);
-            if (!texture.Bounds.Contains(rectangle))
-            {
-                rectangle = new Rectangle(0, 0, textureSize, textureSize);
-            }
-
-            b.Draw(texture, new Rectangle(num3 + 4 * Game1.pixelZoom + num5, num4 + 6 * Game1.pixelZoom, 64 * Game1.pixelZoom, 64 * Game1.pixelZoom), new Rectangle?(rectangle), Color.White, 0f,Vector2.Zero,SpriteEffects.None, 0.88f);
-            if (!Game1.options.hardwareCursor)
-            {
-                d.drawMouse(b);
-            }
-            drawFolderName(b,textureSize,x,y);
-        }  
-
-        private void drawShopkeeper(SpriteBatch b, NPC c)
-        {
-            Texture2D texture = TextureLoader.getPortrait(c.name);
-
-            int textureSize = Math.Max(texture.Width / 2, 64);
-            b.Draw(texture, new Rectangle((activeShop.xPositionOnScreen - 80 * Game1.pixelZoom + Game1.pixelZoom * 5), (activeShop.yPositionOnScreen + Game1.pixelZoom * 5), 64 * Game1.pixelZoom, 64 * Game1.pixelZoom), new Rectangle?(new Rectangle(0, 0, textureSize, textureSize)), Color.White, 0.0f, Vector2.Zero, SpriteEffects.None, 0.92f);
         }
 
         private void ControlEvents_KeyPressed(object sender, EventArgsKeyPressed e)
@@ -134,13 +95,21 @@ namespace Portraiture
             if (e.KeyPressed == config.changeKey && Game1.activeClickableMenu is DialogueBox d && d.isPortraitBox() && Game1.currentSpeaker is NPC cs)
             {
                 if (d.width < 107 * Game1.pixelZoom * 3 / 2 || Helper.Reflection.GetPrivateValue<bool>(d, "transitioning") || Helper.Reflection.GetPrivateValue<bool>(d, "isQuestion"))
-                {
                     return;
-                }
 
                 TextureLoader.nextFolder();
                 displayAlpha = 2;
             }
+
+        }
+
+        private void MenuEvents_MenuChanged(object sender, EventArgsClickableMenuChanged e)
+        {
+            if (Game1.activeClickableMenu is ShopMenu s && s.portraitPerson is NPC c && c.Portrait is Texture2D t && Game1.options.showMerchantPortraits)
+                GraphicsEvents.OnPostRenderGuiEvent += GraphicsEvents_OnPostRenderGuiEvent;
+
+            if (Game1.activeClickableMenu is DialogueBox d && d.isPortraitBox() && Game1.currentSpeaker is NPC ch && ch.Portrait is Texture2D ct)
+                GraphicsEvents.OnPostRenderGuiEvent += GraphicsEvents_OnPostRenderGuiEvent;
 
         }
 
@@ -150,47 +119,16 @@ namespace Portraiture
             GraphicsEvents.OnPostRenderGuiEvent -= GraphicsEvents_OnPostRenderGuiEvent;
         }
 
-
-        private void MenuEvents_MenuChanged(object sender, EventArgsClickableMenuChanged e)
-        {
-            
-
-            if(Game1.currentSpeaker is NPC n && n.Portrait is Texture2D nt)
-            {
-                TextureLoader.loadVanillaPortraits();
-            }
-
-
-            if (Game1.activeClickableMenu is ShopMenu s && s.portraitPerson is NPC c && c.Portrait is Texture2D t && Game1.options.showMerchantPortraits)
-            {
-                activeShop = s;
-                TextureLoader.pushToVanilla(c);
-                GraphicsEvents.OnPostRenderGuiEvent += GraphicsEvents_OnPostRenderGuiEvent;
-            }
-
-            if (Game1.activeClickableMenu is DialogueBox d && d.isPortraitBox() && Game1.currentSpeaker is NPC ch && ch.Portrait is Texture2D ct)
-            {
-                activeDialogueBox = d;
-                
-                TextureLoader.pushToVanilla(ch);
-                GraphicsEvents.OnPostRenderGuiEvent += GraphicsEvents_OnPostRenderGuiEvent;
-            }
-
-        }
-
         private void GraphicsEvents_OnPostRenderGuiEvent(object sender, EventArgs e)
         {
-            if (Game1.activeClickableMenu is ShopMenu s && s.portraitPerson is NPC c && c.Portrait is Texture2D && Game1.options.showMerchantPortraits && Game1.viewport.Width > 800)
-            {
-                drawShopkeeper(Game1.spriteBatch, c);
-            }
-
             if (Game1.activeClickableMenu is DialogueBox d && d.isPortraitBox() && Game1.currentSpeaker is NPC cs)
             {
-                drawPortraiture(Game1.spriteBatch, d, cs);
+                int x = Helper.Reflection.GetPrivateValue<int>(d, "x");
+                int y = Helper.Reflection.GetPrivateValue<int>(d, "y");
+                drawFolderName(Game1.spriteBatch, x, y);                
             }
         }
 
-       
+
     }
 }
