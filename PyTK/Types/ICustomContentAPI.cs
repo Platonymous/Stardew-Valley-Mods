1fad14a666aa3a7d9ab17c4abd93394658c5763f
Platonymous <david@millionideas.de>
2018-01-19 05:10:12 +0100

    Added ICustomContentAPI

---
+++ PyTK/Types/ICustomContentAPI.cs
@@ -0,0 +1,12 @@
+﻿using StardewModdingAPI;
+using StardewValley;
+using System.Collections.Generic;
+
+namespace PyTK.Types
+{
+    public interface ICustomContentAPI
+    {
+        Item getCustomObject(string id);
+        void addContentPack(string folderName, string fileName, IModHelper helper = null, Dictionary<string,string> options = null);
+    }
+}
