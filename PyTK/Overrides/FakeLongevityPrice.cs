783113232d253d8db1e52db11fbc0b2483ec06d3
Platonymous <david@millionideas.de>
2018-01-20 19:03:05 +0100

    Added Longevity Fix

---
+++ PyTK/Overrides/FakeLongevityPrice.cs
@@ -0,0 +1,12 @@
+﻿using StardewValley.Objects;
+
+namespace PyTK.Overrides
+{
+    internal class FakeLongevityPrice
+    {
+        public void UpdateItem(int itemIndex, int updateType, Chest chest = null)
+        {
+
+        }
+    }
+}
