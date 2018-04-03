ebcbb117bb798162d8b2206d79f7af32c7db8cf2
Platonymous <david@millionideas.de>
2018-04-03 22:44:39 +0200

    Fixed sprinklers

--- NoSoilDecayRedux/NoSoilDecayReduxMod.cs
+++ NoSoilDecayRedux/NoSoilDecayReduxMod.cs
@@ -8,53 +8,131 @@ using System.Collections.Generic;
 using System.Linq;
 using PyTK;
 using PyTK.Extensions;
-using PyTK.Types;
+using Harmony;
+using System.Reflection;
 
 namespace NoSoilDecayRedux
 {
     public class NoSoilDecayReduxMod : Mod
     {
-        private Dictionary<GameLocation, List<Vector2>> hoeDirtChache;
+        private static Dictionary<GameLocation, List<Vector2>> hoeDirtChache;
+        private static SaveTiles savetiles;
+        private static string player;
+        internal static IMonitor monitor;
+        internal static IModHelper helper;
 
         public override void Entry(IModHelper helper)
         {
+            monitor = Monitor;
+            NoSoilDecayReduxMod.helper = Helper;
+            savetiles = Helper.ReadConfig<SaveTiles>();
             SaveEvents.AfterLoad += SaveEvents_AfterLoad;
+            TimeEvents.AfterDayStarted += (s,e) => restoreHoeDirt();
+            harmonyFix();
         }
 
-        private void SaveEvents_AfterLoad(object sender, System.EventArgs e)
+        private void harmonyFix()
+        {
+            HarmonyInstance instance = HarmonyInstance.Create("Platonymous.NoSoilDecayRedux");
+            instance.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        private static void SaveEvents_AfterLoad(object sender, System.EventArgs e)
+        {
+            player = Game1.player.name + "_" + Game1.uniqueIDForThisGame.ToString();
+            loadHoeDirt();
+            restoreHoeDirt();
+        }
+
+        private static void restoreHoeDirt()
+        {
+            foreach (GameLocation l in hoeDirtChache.Keys)
+                foreach (Vector2 v in hoeDirtChache[l])
+                {
+                    if (!l.terrainFeatures.ContainsKey(v))
+                        l.terrainFeatures.Add(v, null);
+
+                    if (!(l.terrainFeatures[v] is HoeDirt))
+                    {
+                        l.terrainFeatures[v] = Game1.isRaining ? new HoeDirt(1) : new HoeDirt(0);
+                        if (l.objects.ContainsKey(v) && l.objects[v] is SObject o && (o.name.Equals("Weeds") || o.name.Equals("Stone") || o.name.Equals("Twig")))
+                            l.objects.Remove(v);
+                    }
+
+                    foreach (SObject o in l.objects.Values)
+                        if (o.name.Contains("Sprinkler"))
+                            o.DayUpdate(l);
+
+                }
+        }
+
+        internal static void saveHoeDirt()
         {
             hoeDirtChache = new Dictionary<GameLocation, List<Vector2>>();
-            foreach (var location in PyUtils.getAllLocationsAndBuidlings())
+            foreach (GameLocation location in PyUtils.getAllLocationsAndBuidlings())
             {
+                if (!hoeDirtChache.ContainsKey(location))
+                    hoeDirtChache.Add(location, new List<Vector2>());
+
                 hoeDirtChache[location] = location.terrainFeatures
                     .Where(t => t.Value is HoeDirt)
                     .Select(t => t.Key).ToList();
             }
 
-            new TerrainSelector<HoeDirt>().whenAddedToLocation((gl, list) => list.ForEach(v => hoeDirtChache[gl].AddOrReplace(v)));
-            new TileLocationSelector((l, v) => hoeDirtChache[l].Contains(v)).whenRemovedFromLocation(restoreTiles);
+            string savestring = string.Join(";",hoeDirtChache.toList(l => l.Key.name + ":" + string.Join(",", l.Value.toList(v => $"{v.X}-{v.Y}"))));
 
-            /* Legacy Fix */
-            "Town".toLocation().objects.Remove(new Vector2(2, 0));
+            int index = savetiles.save.FindIndex(s => s.StartsWith(player + ">"));
+
+            if (index == -1)
+                savetiles.save.Add(player + ">" + savestring);
+            else
+                savetiles.save[index] = player + ">" + savestring;
+
+            helper.WriteConfig(savetiles);
         }
 
-        private void restoreTiles(GameLocation l, List<Vector2> list)
+        private static void loadHoeDirt()
         {
-            if (l != Game1.currentLocation)
+            hoeDirtChache = new Dictionary<GameLocation, List<Vector2>>();
+
+            if (savetiles.save == null)
+                savetiles.save = new List<string>();
+
+            int index = savetiles.save.FindIndex(s => s.StartsWith(player + ">"));
+            if (index != -1)
             {
-                foreach (Vector2 v in list)
+                string savestring = savetiles.save[index];
+                savestring.Replace(player + ">", "");
+                List<string> locationstrings = new List<string>(savestring.Split(';'));
+                foreach(string loc in locationstrings)
                 {
-                    l.terrainFeatures[v] = Game1.isRaining ? new HoeDirt(1) : new HoeDirt(0);
-                    if (l.objects.ContainsKey(v) && l.objects[v] is SObject o && 
-                        (o.name.Equals("Weeds") || o.name.Equals("Stone") || o.name.Equals("Twig")))
-                        l.objects.Remove(v);
+                    string locationname = loc.Split(':')[0];
+                    loc.Replace(locationname + ":", "");
+                    List<string> vstrings = new List<string>(loc.Split(','));
+                    GameLocation location = PyUtils.getAllLocationsAndBuidlings().Find(e => e.name == locationname);
+
+                    if (location != null && !hoeDirtChache.ContainsKey(location))
+                        hoeDirtChache.Add(location, new List<Vector2>());
+
+                    foreach(string v in vstrings)
+                    {
+                        string[] split = v.Split('-');
+
+                        if (split.Length < 2)
+                            continue;
+
+                        int x = -1;
+                        int y = -1;
+
+                        int.TryParse(split[0], out x);
+                        int.TryParse(split[1], out y);
+
+                        if (x >= 0 && y >= 0)
+                            hoeDirtChache[location].AddOrReplace(new Vector2(x, y));
+                    }
                 }
             }
-            else
-            {
-                foreach (Vector2 v in list)
-                    hoeDirtChache[l].Remove(v);
-            }
+            helper.WriteConfig(savetiles);
         }
 
     }
---
+++ NoSoilDecayRedux/SleepFix.cs
@@ -0,0 +1,22 @@
+﻿using Harmony;
+using PyTK;
+using StardewValley;
+using System.Reflection;
+
+namespace NoSoilDecayRedux
+{
+    [HarmonyPatch]
+        internal class SleepFix
+    {
+        internal static MethodInfo TargetMethod()
+        {
+            return AccessTools.Method(PyUtils.getTypeSDV("GameLocation"), "answerDialogue");
+        }
+
+        internal static void Postfix(GameLocation __instance, ref Response answer)
+        {
+            if (__instance.lastQuestionKey.ToLower() == "sleep" && answer.responseKey.ToLower() == "yes")
+                NoSoilDecayReduxMod.saveHoeDirt();
+        }
+    }
+}
