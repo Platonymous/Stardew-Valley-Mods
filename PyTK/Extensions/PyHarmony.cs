559f9acce717fbd3e4b972c013c818ed8113ee73
Platonymous <david@millionideas.de>
2018-06-08 07:04:36 +0200

    Added Patch Extensions

---
+++ PyTK/Extensions/PyHarmony.cs
@@ -0,0 +1,79 @@
+﻿using Harmony;
+using StardewModdingAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace PyTK.Extensions
+{
+    public static class PyHarmony
+    {
+        private static Dictionary<string, HarmonyInstance> harmonyInstances = new Dictionary<string, HarmonyInstance>();
+
+        public static void PatchBase(this Type type, IModHelper helper)
+        {
+            type.PatchType(type.BaseType, helper);
+        }
+
+        public static void PatchBase(this Type type, string harmonyId)
+        {
+            type.PatchType(type.BaseType, harmonyId);
+        }
+
+        public static void PatchType(this Type type, Type typeToPatch, IModHelper helper)
+        {
+            type.PatchType(typeToPatch, "Platonymous.PyTK.PyHarmony." + helper.ModRegistry.ModID);
+        }
+
+        public static void PatchType(this Type type, Type typeToPatch, string harmonyId)
+        {
+            if (!harmonyInstances.ContainsKey(harmonyId))
+                harmonyInstances.Add(harmonyId, HarmonyInstance.Create(harmonyId));
+
+            HarmonyInstance harmony = harmonyInstances[harmonyId];
+
+            List<MethodInfo> originals = typeToPatch.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static).ToList();
+
+            foreach (MethodInfo method in originals)
+            {
+                MethodInfo[] preMethods = type.GetMethods(BindingFlags.Public | BindingFlags.Static).Where(m => m.Name == "Prefix_" + method.Name).ToArray();
+                MethodInfo[] postMethods = type.GetMethods(BindingFlags.Public | BindingFlags.Static).Where(m => m.Name == "Postfix_" + method.Name).ToArray();
+
+                MethodInfo preMethod = null;
+                MethodInfo postMethod = null;
+
+                if (preMethods.Length == 1)
+                    preMethod = preMethods.First();
+
+                if (postMethods.Length == 1)
+                    postMethod = postMethods.First();
+
+                if (preMethods.Length > 1)
+                {
+                    List<String> paramter = method.GetParameters().toList(p => p.Name + ":" + p.ParameterType);
+                    preMethod = preMethods.ToList().Find(m =>
+                    {
+                        List<String> mParamter = m.GetParameters().Where(p => !p.Name.Contains("__")).ToArray().toList(p => p.Name + ":" + p.ParameterType);
+                        return mParamter == paramter;
+                    });
+                }
+
+                if (postMethods.Length > 1)
+                {
+                    List<String> paramter = method.GetParameters().toList(p => p.Name + ":" + p.ParameterType);
+                    postMethod = postMethods.ToList().Find(m =>
+                    {
+                        List<String> mParamter = m.GetParameters().Where(p => !p.Name.Contains("__")).ToArray().toList(p => p.Name + ":" + p.ParameterType);
+                        return mParamter == paramter;
+                    });
+                }
+
+                if (postMethod != null || preMethod != null)
+                    harmony.Patch(method, preMethod == null ? null : new HarmonyMethod(preMethod), postMethod == null ? null : new HarmonyMethod(postMethod));
+
+            }
+        }
+    }
+
+}
