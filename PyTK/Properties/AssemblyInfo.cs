72dc9ffa2ea0b7d1146fcf443aced2543d98c9f2
Platonymous <david@millionideas.de>
2018-06-09 23:55:22 +0200

    Added icon

--- PyTK/Properties/AssemblyInfo.cs
+++ PyTK/Properties/AssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PyTK")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Modding Toolkit for Stardew Valley")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Platonymous")]
 [assembly: AssemblyProduct("PyTK")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
