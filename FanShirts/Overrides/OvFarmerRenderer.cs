bcbbb318c13e58939da8433bcf40b53407612929
Platonymous <david@millionideas.de>
2018-06-14 11:38:22 +0200

    Fixed base shirt

--- FanShirts/Overrides/OvFarmerRenderer.cs
+++ FanShirts/Overrides/OvFarmerRenderer.cs
@@ -20,13 +20,13 @@ namespace FanShirts.Overrides
 
             int id = FanShirtsMod.playerBaseJerseys[who.UniqueMultiplayerID];
 
-            if (who.shirt.Value != id)
-                who.changeShirt(id);
+            if (who.shirt.Value != id - 1)
+                who.changeShirt(id - 1);
 
             if (FarmerRenderer.shirtsTexture is ScaledTexture2D st && st.DestinationPositionAdjustment == Vector2.Zero)
             {
-                Rectangle sr = Game1.getSourceRectForStandardTileSheet(FanShirtsMod.vanillaShirts, id, 8, 32);
-                st.DestinationPositionAdjustment = new Vector2(0, -(80 + st.Scale/2 * 8));
+                Rectangle sr = Game1.getSourceRectForStandardTileSheet(FanShirtsMod.vanillaShirts, id - 1, 8, 32);
+                st.DestinationPositionAdjustment = new Vector2(0, -(80 + st.Scale / 2 * 8));
 
                 st.SourcePositionAdjustment = new Vector2(-(sr.X * st.Scale), -(sr.Y * st.Scale));
             }
