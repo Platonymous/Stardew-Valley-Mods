7967cd77df2ace2acaf6435973f5980d38bb3dc3
David <david@millionideas.de>
2020-02-09 23:50:47 +0100

    Fix TV Event

--- PyTK/Overrides/OvTV.cs
+++ PyTK/Overrides/OvTV.cs
@@ -3,6 +3,7 @@ using StardewValley.Objects;
 using System.Reflection;
 using SFarmer = StardewValley.Farmer;
 using PyTK.CustomTV;
+using StardewValley;
 
 namespace PyTK.Overrides
 {
@@ -18,11 +19,17 @@ namespace PyTK.Overrides
 
             internal static bool Prefix(ref TV __instance, SFarmer who, bool justCheckingForActivity)
             {
+                if (Game1.Date.Season == "fall" && Game1.Date.DayOfMonth == 26 && (Game1.stats.getStat("childrenTurnedToDoves") > 0U && !who.mailReceived.Contains("cursed_doll")))
+                    return true;
+
                 return false;
             }
 
             internal static void Postfix(ref bool __result, ref TV __instance, SFarmer who, bool justCheckingForActivity)
             {
+                if (Game1.Date.Season == "fall" && Game1.Date.DayOfMonth == 26 && (Game1.stats.getStat("childrenTurnedToDoves") > 0U && !who.mailReceived.Contains("cursed_doll")))
+                    return;
+
                 CustomTVMod.checkForAction(__instance, who, justCheckingForActivity);
                 __result = true;
             }
