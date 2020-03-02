f8901a3cdfe9299d08ba2989e0e95c8ec4bf0639
David <david@millionideas.de>
2020-03-03 00:46:10 +0100

    Last update

--- ScaleUp/ScaleUpMod.cs
+++ ScaleUp/ScaleUpMod.cs
@@ -27,45 +27,7 @@ namespace ScaleUp
             helper.Content.AssetEditors.Add(new Scaler(helper));
             HarmonyInstance harmony = HarmonyInstance.Create("Platonymous.ScaleUp");
             harmony.Patch(AccessTools.DeclaredMethod(typeof(Texture2D), "FromStream", new Type[] { typeof(GraphicsDevice), typeof(Stream) }), postfix: new HarmonyMethod(this.GetType().GetMethod("LoadFix", BindingFlags.Public | BindingFlags.Static)));
-            harmony.Patch(
-                original: AccessTools.Method(Type.GetType("StardewModdingAPI.Framework.Content.AssetDataForImage, StardewModdingAPI"), "PatchImage"),
-                prefix: new HarmonyMethod(this.GetType().GetMethod("PatchImage", BindingFlags.Public | BindingFlags.Static))
-
-            );
             loadContentPacks();
-        }
-
-        public static void PatchImage(IAssetDataForImage __instance, Texture2D source, Rectangle? sourceArea, Rectangle? targetArea, PatchMode patchMode)
-        {
-            if (source is ScaledTexture2D scaled)
-            {
-                var a = new Rectangle(0, 0, __instance.Data.Width, __instance.Data.Height);
-                var s = new Rectangle(0, 0, source.Width, source.Height);
-                var sr = !sourceArea.HasValue ? s : sourceArea.Value;
-                var tr = !targetArea.HasValue ? sr : targetArea.Value;
-
-                if (a == tr && patchMode == PatchMode.Replace)
-                {
-                   __instance.ReplaceWith(source);
-                   return;
-                }
-
-                if (patchMode == PatchMode.Overlay)
-                    scaled.AsOverlay = true;
-
-                if (scaled.AsOverlay)
-                {
-                    Color[] data = new Color[(int)(tr.Width) * (int)(tr.Height)];
-                    __instance.Data.getArea(tr).GetData(data);
-                    scaled.SetData<Color>(data);
-                }
-
-                if (__instance.Data is MappedTexture2D map)
-                    map.Set(tr, scaled);
-                else
-                    __instance.ReplaceWith(new MappedTexture2D(__instance.Data, new Dictionary<Rectangle?, Texture2D>() { { tr, scaled } }));
-
-            }
         }
 
         public static void LoadFix(Stream stream, ref Texture2D __result)
@@ -78,60 +40,33 @@ namespace ScaleUp
                 int tileWidth = 0;
                 int tileHeight = 0;
                 int fps = 0;
-                string dir = Path.GetDirectoryName(fs.Name);
                 bool loop = true;
-                string dataFile = Path.Combine(dir, key + ".su.json");
 
-                if (File.Exists(dataFile))
-                {
-                    ScaleUpData data = Newtonsoft.Json.JsonConvert.DeserializeObject<ScaleUpData>(File.ReadAllText(dataFile));
+                scaled = Regex.IsMatch(key, scalePattern);
+                animated = Regex.IsMatch(key, animPattern);
 
-                    if (data is ScaleUpData)
-                    {
-                        if (data.Scale != 1f)
-                        {
-                            scale = data.Scale;
-                            scaled = true;
-                        }
+                if (!scaled && !animated)
+                    return;
 
-                        if (data.Animation is Animation anim)
-                        {
-                            tileHeight = anim.FrameHeight;
-                            tileWidth = anim.FrameWidth;
-                            fps = anim.FPS;
-                            loop = anim.Loop;
-                            animated = true;
-                        }
-                    }
-                }
-                else
-                {
-                    scaled = Regex.IsMatch(key, scalePattern);
-                    animated = Regex.IsMatch(key, animPattern);
+                if (!(animated && Regex.Match(key, animPattern) is Match ma
+                    && int.TryParse(ma.Groups[1].Value, out tileWidth)
+                    && int.TryParse(ma.Groups[2].Value, out tileHeight)
+                    && int.TryParse(ma.Groups[3].Value, out fps)))
+                    animated = false;
 
-                    if (!scaled && !animated)
-                        return;
-
-                    if (!(animated && Regex.Match(key, animPattern) is Match ma
-                        && int.TryParse(ma.Groups[1].Value, out tileWidth)
-                        && int.TryParse(ma.Groups[2].Value, out tileHeight)
-                        && int.TryParse(ma.Groups[3].Value, out fps)))
-                        animated = false;
-
-                    if (!(scaled && Regex.Match(key, scalePattern) is Match m && float.TryParse(m.Groups[1].Value, out scale)))
-                        scaled = false;
-                }
-
-                if (scale == 1f)
+                if (!(scaled && Regex.Match(key, scalePattern) is Match m && float.TryParse(m.Groups[1].Value, out scale)))
                     scaled = false;
 
-                if (tileHeight == -1)
+                if (scaled && scale == 1f)
+                    scaled = false;
+
+                if (animated && tileHeight == -1)
                     tileHeight = __result.Height;
 
-                if (tileWidth == -1)
+                if (animated && tileWidth == -1)
                     tileWidth = __result.Width;
 
-                if (tileWidth == __result.Width && tileHeight == __result.Height)
+                if (animated && tileWidth == __result.Width && tileHeight == __result.Height)
                     animated = false;
 
                 if (!scaled && !animated)
