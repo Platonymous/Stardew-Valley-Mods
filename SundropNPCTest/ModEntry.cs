a6c111d77956ba76d232f5a7f6664b0a151e4aca
David <david@millionideas.de>
2021-02-05 17:11:05 +0100

    ...

--- SundropNPCTest/ModEntry.cs
+++ SundropNPCTest/ModEntry.cs
@@ -11,13 +11,6 @@ namespace SundropNPCTest
         {
             SubTypePatcher.Patch<NPC, SundropNPC>();
             helper.Events.GameLoop.DayStarted += GameLoop_DayStarted;
-            helper.Events.GameLoop.DayEnding += GameLoop_DayEnding;
-        }
-
-        private void GameLoop_DayEnding(object sender, StardewModdingAPI.Events.DayEndingEventArgs e)
-        {
-            Game1.removeCharacterFromItsLocation("Pierre");
-            Game1.getLocationFromName("SeedShop").addCharacter(new NPC(new AnimatedSprite("Characters\\Pierre", 0, 16, 32), new Vector2(256f, 1088f), "SeedShop", 2, "Pierre", datable: false, null, Game1.content.Load<Texture2D>("Portraits\\Pierre")));
         }
 
         private void GameLoop_DayStarted(object sender, StardewModdingAPI.Events.DayStartedEventArgs e)
