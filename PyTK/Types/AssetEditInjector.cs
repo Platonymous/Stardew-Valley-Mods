89290567e76dde28dc3f0c2cb9f29d8f2cb1dc68
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2022-05-23 20:53:13 -0400

    split AssetInjector into edit & load injectors, remove unused fields
    This avoids runtime errors if you call injectLoad() with a non-IAssetInfo source, and simplifies migrating to the new content API. No other mods use this API based on a search across open-source mod repos.

---
+++ PyTK/Types/AssetEditInjector.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using StardewModdingAPI;
+
+namespace PyTK.Types
+{
+    public class AssetEditInjector<TAsset, TSource> : IAssetEditor
+    {
+        private readonly Func<TSource, TAsset> asset;
+        private readonly Func<IAssetName, bool> predicate;
+
+        public void Invalidate()
+        {
+            PyTKMod._helper.GameContent.InvalidateCache(a => predicate(a.Name));
+        }
+
+        public AssetEditInjector(string assetName, TAsset asset)
+        {
+            this.asset = _ => asset;
+            predicate = a => a.IsEquivalentTo(assetName, useBaseName: true);
+        }
+
+        public AssetEditInjector(string assetName, Func<TSource, TAsset> asset)
+        {
+            this.asset = asset;
+            predicate = a => a.IsEquivalentTo(assetName, useBaseName: true);
+        }
+
+        public bool CanEdit<TAssetRequest>(IAssetInfo asset)
+        {
+            return predicate(asset.NameWithoutLocale);
+        }
+
+        public void Edit<TAssetRequest>(IAssetData asset)
+        {
+            if (typeof(TSource) == typeof(IAssetDataForImage))
+                this.asset((TSource)asset.AsImage());
+            else
+                asset.ReplaceWith(this.asset.Invoke(asset.GetData<TSource>()));
+        }
+    }
+}
---
+++ PyTK/Types/AssetLoadInjector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using StardewModdingAPI;
+
+namespace PyTK.Types
+{
+    public class AssetLoadInjector<TAsset> : IAssetLoader
+    {
+        private readonly TAsset asset;
+        private readonly Func<IAssetName, bool> predicate;
+
+        public void Invalidate()
+        {
+            PyTKMod._helper.GameContent.InvalidateCache(a => predicate(a.Name));
+        }
+
+        public AssetLoadInjector(string assetName, TAsset asset)
+        {
+            this.asset = asset;
+            predicate = a => a.IsEquivalentTo(assetName, useBaseName: true);
+        }
+
+        public bool CanLoad<TAssetRequest>(IAssetInfo asset)
+        {
+            return predicate(asset.NameWithoutLocale);
+        }
+
+        public TAssetRequest Load<TAssetRequest>(IAssetInfo asset)
+        {
+            return (TAssetRequest)(object)this.asset;
+        }
+    }
+}
